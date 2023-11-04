a5eb9f0bb3e00f0b6ef23b7652b4aa6644a457cb
Merwane Bouri <115326187+MerwaneBouri@users.noreply.github.com>
2023-11-04 18:34:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillMember() {
+  var member = document.getElementById("member");
+  var skill = document.getElementById("skill");
+  var experience = document.getElementById("experience");
+
+  member.style.display = "block";
+  skill.style.display = "none";
+  experience.style.display = "none";
+}
